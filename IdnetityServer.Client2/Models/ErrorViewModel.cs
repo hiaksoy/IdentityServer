2d04b6231b535618a1b96cab8477a4c49d202178
hiaksoy <halilibrahimaksoy54@gmail.com>
2024-09-09 11:16:42 +0300

    Add project files.

---
+++ IdnetityServer.Client2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdnetityServer.Client2.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
